611ae99f2f70d04921ad46fbafbd55ba1de8ddbe
yanerto <yasin.ygd001@hotmail.de>
2019-11-17 10:50:13 +0100

    Added Menu to the script User is now able to change save locations, log files Posts are not unsaved by default, can be changed in options Added new sites to whitelist

--- LinkLookup.py
+++ LinkLookup.py
@@ -8,13 +8,17 @@ import os.path
 import time
 import getpass
 
+#Define variables
+saveFolder = "savedImages/"
+logFile = "logs.txt"
+unsaveWhenDownload = False
+
 #Sends query to get OAuth token
 def getAuthToken(username, AppID, AppSecret, password):
 	client_auth = requests.auth.HTTPBasicAuth(AppID, AppSecret)
 	post_data = {"grant_type": "password", "username": "%s" % username, "password": "%s" % password}
-	headerAuth = {"User-Agent": "SavedImagesGrabber/1.1 by Yanerto"}
+	headerAuth = {"User-Agent": "SavedImagesGrabber/1.2 by Yanerto"}
 	token = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headerAuth)
-
 	return token;
 
 #Downloads image from url and saves as fileName
@@ -42,8 +46,9 @@ def unsavePost(id):
 #Saves all posts of page with given listingVariable
 def savePage(listingVariable):
 	#Query Reddit API
-	post_data = {"after": listingVariable}
-	response = requests.get("https://oauth.reddit.com/user/%s/saved" % username, headers=header)
+	if(!unsaveWhenDownload):
+		post_data = {"after": listingVariable}
+	response = requests.get("https://oauth.reddit.com/user/%s/saved" % username, headers=header, params=post_data)
 	dataResponse = response.json()
 	upperLimit = len(dataResponse["data"]["children"])
 
@@ -57,7 +62,7 @@ def savePage(listingVariable):
 		menuListing = "[%d]  " % (x) + url
 		print(menuListing)
 		fileName = "savedImages/" + url.split("/")[-1]
-		whitelist = ['awwni.me', 'i.imgur', 'i.redd.it']
+		whitelist = ['awwni.me', 'i.imgur', 'i.redd.it', 'media.giphy', 'cdn.discordapp']
 
 		#If post URL is in whitelist, aka if post is an image
 		if any(x in url for x in whitelist):
@@ -66,14 +71,78 @@ def savePage(listingVariable):
 			with open("logs.txt", "a") as myFile:
 				textToAttach = fileName[12:] + "                               " + "https://old.reddit.com" + dataResponse["data"]["children"][x]["data"]["permalink"] + " \n"
 				myFile.write(textToAttach)
-			unsavePost(dataResponse["data"]["children"][x]["data"]["name"])
+				if(unsaveWhenDownload):
+					unsavePost(dataResponse["data"]["children"][x]["data"]["name"])
 		else:
 			print("Not in Whitelist")
 
 	return dataResponse["data"]["after"];
 
 
-print("Welcome!")
+def printMainMenu():
+	print(30*"=","MENU",33*"=")
+	print("1. Run")
+	print("2. Options")
+	print("3. Exit")
+	print(69*"=")
+	return;
+
+def OptionMenu():
+	global saveFolder
+	global logFile
+	global unsaveWhenDownload
+	print(30*"=","Options",30*"=")
+	print("1. Change current save Location: %s" % (saveFolder))
+	print("2. Change current log File: %s" % (logFile))
+	print("3. Unsave Posts when downloading?: %s" % (str(unsaveWhenDownload)) )
+	print("4. Back")
+	print(69*"=")
+
+	option = getUserInput("")
+	if option == 1:
+		tmp = input("Please put in relative Path to folder")
+		if(os.path.exists(tmp) and os.path.isdir(tmp)):
+			saveFolder = tmp
+		else:
+			print("Given path is not correct")
+	elif option == 2:
+		tmp = input("Please put in the relative Path to the log file")
+		if(os.path.exists(tmp)):
+			logFile = tmp
+		else:
+			print("Given path is not correct")
+	elif option == 3:
+		if(unsaveWhenDownload):
+			print("Saved posts will not be unsaved when downloading")
+			unsaveWhenDownload = False
+		else:
+			print("Saved posts will be unsaved when downloading")
+			unsaveWhenDownload = True
+	elif option == 4:
+		return;
+
+
+
+def getUserInput(prompt):
+	tmp = -1
+	while tmp == -1:
+		tmp = input(prompt)
+	option = int(tmp)
+	return option;
+
+
+while 1:
+	printMainMenu()
+	option = getUserInput("")
+	if option == 1:
+		print("Run")
+		break;
+	elif option == 2:
+		OptionMenu()
+	elif option == 3:
+		sys.exit("Quitting")
+
+
 
 #Get Credentials of user
 username = ""
@@ -102,7 +171,9 @@ if(len(token.json())) == 1:
 #Ask for pages
 maxPages = input("How many pages to save?")
 listingVariable = ""
+print(token.json())
 header = {"Authorization": "bearer " + token.json()['access_token'], "User-Agent": "SavedImagesGrabber/1.0 by Yanerto"}
 
 for k in range(0, int(maxPages)):
+    print(k)
     listingVariable = savePage(listingVariable)
